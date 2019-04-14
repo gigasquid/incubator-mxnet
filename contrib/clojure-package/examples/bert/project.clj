2841054158b22ce28dd99f3de28bc516857e0c36
gigasquid <cmeier@gigasquidsoftware.com>
2019-04-14 08:54:35 -0400

    add BERT example with fine tuning

--- contrib/clojure-package/examples/bert/project.clj
+++ contrib/clojure-package/examples/bert/project.clj
@@ -17,12 +17,13 @@
 
 
 (defproject bert-qa "0.1.0-SNAPSHOT"
-  :description "BERT QA Example"
+  :description "BERT Examples"
   :plugins [[lein-cljfmt "0.5.7"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.mxnet.contrib.clojure/clojure-mxnet "1.5.0-SNAPSHOT"]
-                 [cheshire "5.8.1"]]
+                 [cheshire "5.8.1"]
+                 [clojure-csv/clojure-csv "2.0.1"]]
   :pedantic? :skip
   :java-source-paths ["src/java"]
-  :main bert-qa.infer
-  :repl-options {:init-ns bert-qa.infer})
+  :main bert.infer
+  :repl-options {:init-ns bert.infer})
---
+++ contrib/clojure-package/examples/bert/src/bert/util.clj
@@ -0,0 +1,35 @@
+(ns bert.util
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [cheshire.core :as json]))
+
+(defn break-out-punctuation [s str-match]
+  (->> (string/split (str s "<punc>") (re-pattern (str "\\" str-match)))
+       (map #(string/replace % "<punc>" str-match))))
+
+(defn break-out-punctuations [s]
+  (if-let [target-char (first (re-seq #"[.,?!]" s))]
+    (break-out-punctuation s target-char)
+    [s]))
+
+(defn tokenize [s]
+  (->> (string/split s #"\s+")
+       (mapcat break-out-punctuations)
+       (into [])))
+
+(defn pad [tokens pad-item num]
+  (if (>= (count tokens) num)
+    tokens
+    (into tokens (repeat (- num (count tokens)) pad-item))))
+
+(defn get-vocab []
+  (let [vocab (json/parse-stream (io/reader "data/vocab.json"))]
+    {:idx->token (get vocab "idx_to_token")
+     :token->idx (get vocab "token_to_idx")}))
+
+(defn tokens->idxs [token->idx tokens]
+  (let [unk-idx (get token->idx "[UNK]")]
+   (mapv #(get token->idx % unk-idx) tokens)))
+
+(defn idxs->tokens [idx->token idxs]
+  (mapv #(get idx->token %) idxs))
