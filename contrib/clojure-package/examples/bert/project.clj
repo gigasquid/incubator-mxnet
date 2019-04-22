410075873ac8990d77fc3e4cf671d1c591bfb16a
gigasquid <cmeier@gigasquidsoftware.com>
2019-04-22 15:39:05 -0400

    restructuring project

--- contrib/clojure-package/examples/bert/project.clj
+++ contrib/clojure-package/examples/bert/project.clj
@@ -17,7 +17,7 @@
 
 
 (defproject bert-qa "0.1.0-SNAPSHOT"
-  :description "BERT QA Example"
+  :description "BERT Examples"
   :plugins [[lein-cljfmt "0.5.7"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.mxnet.contrib.clojure/clojure-mxnet "1.5.0-SNAPSHOT"]
@@ -25,5 +25,5 @@
                  [clojure-csv/clojure-csv "2.0.1"]]
   :pedantic? :skip
   :java-source-paths ["src/java"]
-  :main bert-qa.infer
-  :repl-options {:init-ns bert-qa.infer})
+  :main bert.infer
+  :repl-options {:init-ns bert.infer})
